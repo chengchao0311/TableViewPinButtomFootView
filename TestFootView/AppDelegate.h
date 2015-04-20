175b902e25ce77e17fcab8e8c3af4a12ee8d3d86
Shawn Chan <chengchao0311@gmail.com>
2015-04-20 23:47:22 +0800

    Initial Commit

---
+++ TestFootView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestFootView
+//
+//  Created by Xinqi Chan on 4/20/15.
+//  Copyright (c) 2015 Xinqi Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestFootView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestFootView
+//
+//  Created by Xinqi Chan on 4/20/15.
+//  Copyright (c) 2015 Xinqi Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
